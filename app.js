6398139ea118ae0a7478ef2e43f877bfb771c916
dylan@dylanhulbert.com <dylan@dylanhulbert.com>
2020-12-25 14:07:05 -0800

    updated dir structure

--- app.js
+++ app.js
@@ -3,11 +3,15 @@ var express = require('express');
 var path = require('path');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
+// var pool = require('./database')();  
 
 var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
 
 var app = express();
+
+// app.listen(process.env.PORT || 5050);  
+// console.log("App listening on port 5050"); 
 
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
---
+++ routes.js
@@ -0,0 +1,5 @@
+module.exports = function (app, pool) {  
+app.get('/', function (req, res) {  
+        res.send("Welcome to C-sharp corner Api");  
+    });  
+}; 
